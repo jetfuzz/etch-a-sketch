a33df6948606326cec967d19104ea5fffad37053
jetfuzz <1jsf23@gmail.com>
2024-04-11 09:38:08 -0500

    Create simple 16x16 grid

---
+++ index.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 256; i++) {
+    let div = document.createElement("div");
+    div.style.width = "25px";
+    div.style.height = "25px";
+    div.style.border = "solid black"
+    container.appendChild(div);
+}
