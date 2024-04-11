1b67134b07ad5a66b4a48814c0b32aa79e0c363e
jetfuzz <1jsf23@gmail.com>
2024-04-11 09:47:07 -0500

    Create function for current grid

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement("div");
-    div.style.width = "25px";
-    div.style.height = "25px";
-    div.style.border = "solid black"
-    container.appendChild(div);
+const container = document.getElementById("container");
+
+function createGrid() {
+    for(let i = 0; i < 256; i++) {
+        let div = document.createElement("div");
+        div.style.width = "25px";
+        div.style.height = "25px";
+        div.style.border = "solid black";
+        div.style.borderRadius = "4px"
+        container.appendChild(div);
+    }
 }
+
+createGrid(container);
