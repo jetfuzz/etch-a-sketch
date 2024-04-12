85752bbcdd01b0256e3451dd19183d749f0c964f
jetfuzz <1jsf23@gmail.com>
2024-04-12 14:51:39 -0500

    Change CSS for grid to remain a fixed size

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ function createGrid(rows, columns) {
         for (let j = 0; j < columns; j++) {
             let div = document.createElement("div");
             div.className = "cell";
-            div.style.width = "25px";
-            div.style.height = "25px";
-            div.style.border = "2px solid black";
-            div.style.margin = "1px";
             row.appendChild(div);
         }
     }
